101f5598c77bc818ba79e7c795be57c5db17b1bc
JuryShev <shevrikukoyura@gmail.com>
2020-07-01 14:03:36 +0300

    Correction light

--- Digital/Microcolony_Recognition/Image_preprocessing.py
+++ Digital/Microcolony_Recognition/Image_preprocessing.py
@@ -17,14 +17,32 @@ def color_to_gray(path_image_c , path_image_g):
         name_n_imj = name_imj[:k] + png
         image = cv2.imread(openimj)
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray_image_est = gray_image.astype(np.float32).reshape(-1, 512 * 640)
+        gray_image_est=np.mean(gray_image_est)
+
+
         # if(gray_image.shape[0]<cowl):
         #     cowl=gray_image.shape[0]
         # if (gray_image.shape[1]<rowl):
         #     rowl = gray_image.shape[1]
         #gray_image = cv2.resize(gray_image, (rowl, cowl))
-        if name_n_imj=='250.png':
-            gray_image=dark_image(img=gray_image)
-        #cv2.imwrite(os.path.join(path_image_g, name_n_imj), gray_image)
+        if gray_image_est>158:
+            #gray_image=dark_image(img=gray_image)
+            gamma = 150 * 0.309 / gray_image_est
+
+            gray_image=adjust_gamma(image=gray_image, gamma=gamma)
+
+           # gray_image_est=gray_image.astype(np.float32).reshape(-1, 512 * 640)
+            #gray_image_dark_est=gray_image_dark.astype(np.float32).reshape(-1, 512 * 640)
+
+            #print("mean_L=",np.mean(gray_image_est))
+            #print("mean_D=", np.mean(gray_image_dark_est))
+
+
+
+            #viewTwoImage(gray_image,gray_image_dark)
+
+        cv2.imwrite(os.path.join(path_image_g, name_n_imj), gray_image)
 
     print("minimal cowl=", cowl)
     print("minimal rowl=", rowl)
@@ -69,5 +87,18 @@ def dark_image(img):
     #viewImage(dark_img, "image_l2")
 
     return dark_img
+# import the necessary packages
 
 
+def adjust_gamma(image, gamma=1.0):
+    # build a lookup table mapping the pixel values [0, 255] to
+	# their adjusted gamma values
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+		for i in np.arange(0, 256)]).astype("uint8")
+    # apply gamma correction using the lookup table
+    return cv2.LUT(image, table)
+
+def winVar(img, wlen):
+  wmean, wsqrmean = (cv2.boxFilter(x, -1, (wlen, wlen),borderType=cv2.BORDER_REFLECT) for x in (img, img*img))
+  return wsqrmean - wmean*wmean
