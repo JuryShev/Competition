1e649ab1683cbac3a53463aa1946ea6c5ed0aa7a
JuryShev <shevrikukoyura@gmail.com>
2020-07-02 22:22:43 +0300

    Creat CutStickImage Creat comp_mean_light

--- Digital/Microcolony_Recognition/Image_preprocessing.py
+++ Digital/Microcolony_Recognition/Image_preprocessing.py
@@ -11,14 +11,30 @@ def color_to_gray(path_image_c , path_image_g):
     filename_list = glob.glob(path_image_c)
     for openimj in filename_list:
         # print(openimj)
-        name_imj = base = os.path.basename(openimj)
+        name_imj = os.path.basename(openimj)
         k = len(name_imj)
         k = k - 3
         name_n_imj = name_imj[:k] + png
         image = cv2.imread(openimj)
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        gray_image_est = gray_image.astype(np.float32).reshape(-1, 512 * 640)
-        gray_image_est=np.mean(gray_image_est)
+        gray_image_orig=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray_image=CutStickImage(gray_image_orig)
+
+        flag_dark=0
+        for i in range(4):
+            gray_image_est=comp_mean_light(img_O=gray_image[i],img_A=gray_image[i],w=gray_image[i].shape[1], h=gray_image[i].shape[0])
+
+            if gray_image_est > 170:
+                flag_dark=1
+                gamma = 150 * 0.309 / gray_image_est
+
+                gray_image[i] = adjust_gamma(image=gray_image[i], gamma=gamma)
+
+                #print("mean_L=", np.mean(gray_image_est))
+                #print("mean_D=", np.mean(gray_image_dark_est))
+
+        gray_image_stick=CutStickImage(gray_image, 0)
+        # if flag_dark==1:
+        #     viewTwoImage(gray_image_orig, gray_image_stick, name=name_n_imj)
 
 
         # if(gray_image.shape[0]<cowl):
@@ -26,23 +42,9 @@ def color_to_gray(path_image_c , path_image_g):
         # if (gray_image.shape[1]<rowl):
         #     rowl = gray_image.shape[1]
         #gray_image = cv2.resize(gray_image, (rowl, cowl))
-        if gray_image_est>158:
-            #gray_image=dark_image(img=gray_image)
-            gamma = 150 * 0.309 / gray_image_est
-
-            gray_image=adjust_gamma(image=gray_image, gamma=gamma)
-
-           # gray_image_est=gray_image.astype(np.float32).reshape(-1, 512 * 640)
-            #gray_image_dark_est=gray_image_dark.astype(np.float32).reshape(-1, 512 * 640)
-
-            #print("mean_L=",np.mean(gray_image_est))
-            #print("mean_D=", np.mean(gray_image_dark_est))
 
 
-
-            #viewTwoImage(gray_image,gray_image_dark)
-
-        cv2.imwrite(os.path.join(path_image_g, name_n_imj), gray_image)
+        cv2.imwrite(os.path.join(path_image_g, name_n_imj),gray_image_stick)
 
     print("minimal cowl=", cowl)
     print("minimal rowl=", rowl)
@@ -51,13 +53,13 @@ def color_to_gray(path_image_c , path_image_g):
 def viewImage(image, name_of_window):
     cv2.namedWindow(name_of_window, cv2.WINDOW_NORMAL)
     cv2.imshow(name_of_window, image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    cv2.waitKey()
 
-def viewTwoImage(img_O, img_A ):
+
+def viewTwoImage(img_O, img_A, name="img" ):
 
     numpy_horizontal_concat = np.concatenate((img_O, img_A), axis=1)
-    cv2.imshow('Numpy Vertical Concat', numpy_horizontal_concat)
+    cv2.imshow(name, numpy_horizontal_concat)
     cv2.waitKey()
 
 def dark_image(img):
@@ -65,15 +67,15 @@ def dark_image(img):
     kernel = np.ones((3, 3), np.float32)
 
     kernel[0][0]=-0.1
-    kernel[0][1]=0.1
+    kernel[0][1]=0.2
     kernel[0][2]=-0.1
 
     kernel[1][0] = 0.1
-    kernel[1][1] = 0.5
+    kernel[1][1] = 1.2
     kernel[1][2] = 0.1
 
     kernel[2][0] = -0.1
-    kernel[2][1] = 0.1
+    kernel[2][1] = 0.2
     kernel[2][2] = -0.1
 
 
@@ -99,6 +101,53 @@ def adjust_gamma(image, gamma=1.0):
     # apply gamma correction using the lookup table
     return cv2.LUT(image, table)
 
-def winVar(img, wlen):
-  wmean, wsqrmean = (cv2.boxFilter(x, -1, (wlen, wlen),borderType=cv2.BORDER_REFLECT) for x in (img, img*img))
-  return wsqrmean - wmean*wmean
+def CutStickImage(img, flag_choise=1):
+    """
+
+    :param img: список или изображение
+    :param flag_choise: 1-режим 0 склеиваем
+    :return:  1 список фрашментов 0 целое изображение
+
+
+    """
+    list_img=[]
+
+
+
+
+
+    if flag_choise==1:
+        width_0 = int(img.shape[0] / 2)
+        height_0 = int(img.shape[1] / 2)
+        list_img.append(img[:width_0,:height_0])
+        list_img.append(img[:width_0, height_0:])
+        list_img.append(img[width_0:, :height_0])
+        list_img.append(img[width_0:, height_0:])
+
+        # viewImage(list_img[0], "1")
+        # viewImage(list_img[1], "2")
+        # viewImage(list_img[2], "3")
+        # viewImage(list_img[3], "4")
+
+        return list_img
+    else:
+        horizontal_concat_H = np.concatenate((img[0], img[1]), axis=1)
+        horizontal_concat_L = np.concatenate((img[2], img[3]), axis=1)
+        img_stick=np.concatenate((horizontal_concat_H,horizontal_concat_L), axis=0)
+       # viewImage(img_stick, "final_stick")
+        return img_stick
+
+
+
+def comp_mean_light(img_O, img_A, w, h, flag_choise=1):
+
+    if flag_choise==1:
+        gray_image_est = img_O.astype(np.float32).reshape(-1, h * w)
+        gray_image_est = np.mean(gray_image_est)
+
+        return gray_image_est
+
+    gray_image_est = img_O.astype(np.float32).reshape(-1, h * w)
+    gray_image_dark_est = img_A.astype(np.float32).reshape(-1, h * w)
+
+
